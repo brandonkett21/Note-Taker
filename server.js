61368c90e93e59b0e012ac53adc1e39ec043cf0e
Brandon Ketterman <brandonkett24@gmail.com>
2020-09-03 19:28:09 -0400

    final commit

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-// Dependencies
-// =============================================================
 var express = require("express");
 var apiRoute = require("./routes/apiRoute");
 var htmlRoute = require("./routes/htmlRoute");
 
-// Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -14,13 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Routes
-// =============================================================
 app.use("/api", apiRoute);
 app.use("/", htmlRoute);
 
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function () {
   console.log(`App listening on http://localhost:${PORT}`);
 });
